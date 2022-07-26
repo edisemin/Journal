386aeb402c2a6b3bc47386886088b5bf3c398d1f
azotamiota <azotamiota@gmail.com>
2022-07-26 18:55:25 +0100

    emojis working!!

--- client/assets/scripts/index.js
+++ client/assets/scripts/index.js
@@ -4,79 +4,15 @@ const input = document.querySelector(".input");
 const main = document.querySelector('#blog-posts')
 const newPostButton = document.getElementById('btn-two')
 
-console.log('hello')
+// const url = "https://community-journaling.herokuapp.com";
+const url = "http://localhost:3000";
+
+
 
 document.addEventListener('submit', (e)=> e.preventDefault())
 newPostButton.addEventListener('click', writeNewPost)
 
-    
 
-///// this will post data to the server////////////////////////
-
-// function postNewPost(form) {
-//    const prePayload = new FormData(form);
-//    const payload = new URLSearchParams(prePayload);
-
-//    //console.log([...payload]);
-
-//    fetch('http://localhost:3000/', {
-//     method: "post",
-//     body: payload, ['key', 'blogpost']
-//   })
-//     .then((res) => res.json())
-//     .then((data) => console.log(data))
-//     .catch((err) => console.log(err));
-// }
-
-
-///// This function adds elements and the data returned from the server
-// function addElements (data) {
-   
-
-//    let commentContainer = document.createElement("div");
-// /////////////////////////////////////// we need to replace input.value with the latest data that was posted////////////
-//    const commentForm = commentContainer.innerHTML =  `<p>${input.value}</p><form id='form'>
-
-//    <input name='userpost' type="text" class="inputTwo" placeholder='write a comment ...'required>
-//    <button type="submit" id='btn'>Post Comment</button></form>` 
-//    return commentForm
-
-// }
- 
-
-// submits the index.html form
-// form.addEventListener("submit", (e) => {
-//     /// call postdata function and pass in the form////////////////////////
-//    PostData(e.target);
-   
-//    content.innerHTML += addElements() 
-
-//    input.value = "";
-  
-// });
-
-
-// submits all other forms using the event.
-// content.addEventListener("submit", (e) => {
- 
-//    PostData(e.target);
-
-//    const targ = e.target.querySelector('.inputTwo').value
-
-//   /// we need to replace targ with the latest data that was posted on the comment reply///////////////////
-//    let html = `<p id='p'>${targ}</p>`;
-//    e.target.previousSibling.insertAdjacentHTML("afterend", html)
-
-//    e.target.querySelector('.inputTwo').value = ''
-// });
-
-
-
-
-
-
-
-/// planning on using this function to make a get request 
 
 function writeNewPost() {
    
@@ -98,7 +34,7 @@ function writeNewPost() {
 /********************************************************************** This is for trying methods (not working now) */
 function loadInitialPage (data) {
   const blogLength = Object.keys(data).length
-  for (let i = 1; i <= blogLength; i++) {
+  for (let i = blogLength; i > 0; i--) {
 
     const blogPostContainer = document.createElement('p')
     blogPostContainer.setAttribute('class', 'content');
@@ -251,8 +187,8 @@ function loadInitialPage (data) {
             } else {
 
                 document.getElementById(`funny-button-${i}`).addEventListener('click', 
-                  function incrementLike() {
-                      console.log('im within addFunny function')
+                  function incrementFunny() {
+                      console.log('im within incrementFunny function')
                       replyForm.appendChild(createFunnyEmoji)
                               
                       const http = new XMLHttpRequest();
@@ -274,12 +210,87 @@ function loadInitialPage (data) {
                   }
               )
                   replyForm.appendChild(createFunnyEmoji)
-                  likeCounter.textContent = data[i]['funny']['number']
+                  funnyCounter.textContent = data[i]['funny']['number']
             }
         console.log('Im the imported funny checker')
         }   
 
         funnyChecker()
+
+        const angryButton = document.createElement('button')
+        replyForm.appendChild(angryButton)
+        angryButton.setAttribute('type', 'submit')
+        angryButton.setAttribute('id', `angry-button-${i}`)
+        angryButton.textContent = 'Angry'
+
+        const createAngryEmoji = document.createElement('img')
+        createAngryEmoji.setAttribute('src', '/client/assets/emojis/angry.svg')
+        createAngryEmoji.setAttribute('class', 'svg-angry')
+
+        const angryCounter = document.createElement('div')
+        blogPostContainer.appendChild(angryCounter)
+        angryCounter.setAttribute('class', 'angry-counter')
+        angryCounter.textContent = data[i]['angry']['number']
+
+        function angryChecker () {
+          if (!data[i]['angry']['is-there']) {
+              console.log('There is no "angry" emoji')
+                      
+              document.getElementById(`angry-button-${i}`).addEventListener('click', 
+                        function addAngry() {
+                        console.log('im within addAngry function')
+                        replyForm.appendChild(createAngryEmoji)
+                          
+                  const http = new XMLHttpRequest();
+                  
+                  const params = `angry-button-${i}=clicked`;
+                  console.log('params:', params) //*****
+                  http.open('POST', url, true);
+                  console.log('after the http open') //*****
+
+                  //Send the proper header information along with the request
+                  http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                  http.onreadystatechange = function() {//Call a function when the state changes.
+                      if(http.readyState == 4 && http.status == 200) {
+                          alert(http.responseText);
+                      }
+                  }
+                  http.send(params);
+                  window.location.reload()
+                  }
+              )
+                      
+            } else {
+
+                document.getElementById(`angry-button-${i}`).addEventListener('click', 
+                  function incrementAngry() {
+                      console.log('im within incrementAngry function')
+                      replyForm.appendChild(createAngryEmoji)
+                              
+                      const http = new XMLHttpRequest();
+                      
+                      const params = `angry-button-${i}=clicked`;
+                      console.log('params:', params) //*****
+                      http.open('POST', url, true);
+                      console.log('after the http open') //*****
+
+                      //Send the proper header information along with the request
+                      http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                      http.onreadystatechange = function() {//Call a function when the state changes.
+                          if(http.readyState == 4 && http.status == 200) {
+                              alert(http.responseText);
+                          }
+                      }
+                      http.send(params);
+                      window.location.reload()
+                  }
+              )
+                  replyForm.appendChild(createAngryEmoji)
+                  angryCounter.textContent = data[i]['angry']['number']
+            }
+        }
+
+        angryChecker()
 
    document.getElementById(`btn-${i}`).addEventListener('click',
    function submitReply() {
@@ -304,12 +315,6 @@ function loadInitialPage (data) {
 /********************************************************************************************* End of trying block*/
 
 
-
-
-
-// const url = "https://community-journaling.herokuapp.com";
-const url = "http://localhost:3000";
-
 function getData() {
 
   fetch(url)
@@ -323,66 +328,5 @@ function getData() {
     })
 }
 
-// async function getData() {
-//   try {
-//     const response = fetch(url);
-//     const data = await response.json();
-    
-//     console.log(data[1].body)
-//   } catch (error) {
-//     alert("Sorry! Your request could not be granted!");
-//   }
-// }
 
 getData()
-
-
-
-
-
-
-/// this is the data being returned 
-
-
-//{
-  // "1":{
-    //  "body":"This is the first comment",
-    //  "replies":[
-    //     "first reply",
-    //     "second reply"
-   //   ],
-    //  "like":{
-   //      "is-there":true,
-   //      "number":1
-   //   },
-   //   "funny":{
-    //     "is-there":true,
-    //     "number":4
-    //  },
-    //  "angry":{
-     //    "is-there":false,
-     //    "number":0
-     // }
-   //},
-   //"2":{
-    //  "body":"This is the second comment",
-    //  "replies":[
-     //    "first reply",
-    //     "second reply",
-    //     "third reply"
-    //  ],
-    //  "like":{
-     //    "is-there":false,
-     //    "number":0
-    //  },
-     // "funny":{
-     //    "is-there":true,
-     //    "number":2
-    //  },
-     // "angry":{
-     //    "is-there":true,
-     //    "number":5
-     // }
-   //}
-//}
-
