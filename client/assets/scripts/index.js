f4a8cca8240b8728241e48fb701daccb40242ee3
azotamiota <azotamiota@gmail.com>
2022-07-28 11:07:10 +0100

    handle merge conflicts

--- client/assets/scripts/index.js
+++ client/assets/scripts/index.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-let button = document.querySelector("#gif-button")
-
-button.addEventListener('click', () => {
-    sendApiRequest()
-})
-
-async function sendApiRequest() {
-    let userInput = document.querySelector("#gif-input").value
-    let response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=zdlCdp0EGvd7yxHXu5B7ywlPueKFWe5w&q=${userInput}`)
-    // console.log(response)
-    let gifs = await response.json()
-    // console.log(gifs)
-    useApiData(gifs)
-};
-
-function useApiData(gifs) {
-    document.querySelector('#post-gif').innerHTML = 
-    `<img src = "${gifs.data[0].images.original.url}">`
-}
-=======
 const form = document.querySelector(".form_main");
 const input = document.querySelector(".input");
 const main = document.querySelector('#blog-posts')
@@ -432,4 +411,3 @@ function getData() {
 
 
 getData()
->>>>>>> a8a02c83db0c92703cea79a8d34917af21c6cee7
